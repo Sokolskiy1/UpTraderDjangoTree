b432d182bffd6a3cfbbec39d91c5d18aff6e6dc3
sokolskiy <vanosocol1@gmail.com>
2025-11-28 01:13:10 +0300

    Add fetch and add html markup

--- menu_tree_solution/views.py
+++ menu_tree_solution/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from menu_tree_solution.models import Menu
+
 
 # Create your views here.
 def menu_tree_view(request):
-   return render(request, 'menu_tree/menu_tree_main.html')
+   menu = Menu.objects.prefetch_related('parent').filter(parent=None)
+   print(menu.first().children.all())
+   return render(request, 'menu_tree/menu_tree_main.html',context={'menu':menu})
