428ec3f43cb185c42540d8a67d679162aad9b322
sokolskiy <vanosocol1@gmail.com>
2025-11-28 00:16:38 +0300

    Add model menu

--- menu_tree_solution/admin.py
+++ menu_tree_solution/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Menu
+from django.db import models
+
 # Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name','parent']
+
--- menu_tree_solution/models.py
+++ menu_tree_solution/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, null=True, blank=True)
+    url = models.CharField(max_length=255,default='')
+
+
+    class Meta:
+        verbose_name = ('menu')
+        verbose_name_plural = ('menus')
+
+    def __str__(self): return self.name
+
