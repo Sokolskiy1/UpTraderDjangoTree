22b591e9b61426b5ae52e2326baa8c7a353f44f0
sokolskiy <vanosocol1@gmail.com>
2025-11-28 00:35:11 +0300

    Menu url + render page   - add view   - add url   - add pattern in templates

--- UpTraderDjango/urls.py
+++ UpTraderDjango/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from menu_tree_solution.views import  menu_tree_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menu_tree/', menu_tree_view, name='menu_tree_main'),
 ]
--- menu_tree_solution/views.py
+++ menu_tree_solution/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def menu_tree_view(request):
+   return render(request, 'menu_tree/menu_tree_main.html')
