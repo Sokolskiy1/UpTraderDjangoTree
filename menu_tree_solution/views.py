4efc2c8b7e418fde0e97d7689a56efe86f681857
sokolskiy <vanosocol1@gmail.com>
2025-11-28 04:03:01 +0300

    test

--- menu_tree_solution/views.py
+++ menu_tree_solution/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
 from menu_tree_solution.models import Menu
-
+import sqlite3
 
 # Create your views here.
 def menu_tree_view(request,name_item_tree=None):
    print(name_item_tree)
    if (name_item_tree):
       menu = Menu.objects.prefetch_related('parent').filter(name=name_item_tree)
+
    else:
       menu = Menu.objects.prefetch_related('parent').filter(parent=None)
-   print(menu.first().children.all())
+
+   print(menu)
+   for item in menu.values():
+      print(item)
    return render(request, 'menu_tree/menu_tree_main.html',context={'menu':menu})
